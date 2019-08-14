0256b5eab05f23838bf77164b1f76ec221b74a59
Andrey Lukashov <andrey.lukashov@gmail.com>
2019-08-14 12:34:37 -0700

    Refactored model

--- app/models.py
+++ app/models.py
@@ -83,7 +83,9 @@ class Driver(db.Model):
     __tablename__ = 'driver'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
+    first_name = db.Column(db.String(100), nullable=False)
+    middle_name = db.Column(db.String(100), nullable=True)
+    last_name = db.Column(db.String(100), nullable=False)
     dob = db.Column(db.Date, nullable=False)
 
     def __init__(self, name, dob):
@@ -107,6 +109,8 @@ class Driver(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "name": self.name,
+            "first_name": self.first_name,
+            "middle_name": self.middle_name,
+            "last_name": self.last_name,
             "dob": self.dob.strftime('%d/%m/%Y')
         }
